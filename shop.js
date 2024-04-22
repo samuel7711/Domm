2d1ab8098962f43bf471d83699abe8962e96ea67
samuel7711 <160052004+samuel7711@users.noreply.github.com>
2024-04-21 23:47:06 -0500

    Update shop.js

--- shop.js
+++ shop.js
@@ -56,8 +56,8 @@ const nombrePro2 = document.createElement("h3");
 nombrePro2.textContent = producto2.nombre;
 producto2.appendChild(nombrePro2);
 
-const foto = document.getElementById("image");
-producto2.appendChild(foto);
+const foto2 = document.getElementById("image");
+producto2.appendChild(foto2);
     
 const precioPro2 = document.createElement("p");
 precioPro2.textContent = `Precio: $${producto2.precio}`;
@@ -76,8 +76,8 @@ const nombrePro3 = document.createElement("h3");
 nombrePro3.textContent = producto3.nombre;
 producto3.appendChild(nombrePro3);
 
-const foto2 = document.getElementById("image");
-producto3.appendChild(foto2);
+const foto3 = document.getElementById("image");
+producto3.appendChild(foto3);
     
 const precioPro3 = document.createElement("p");
 precioPro3.textContent = `Precio: $${producto3.precio}`;
@@ -86,6 +86,44 @@ producto3.appendChild(precioPro3);
 const compra3= document.getElementById("boton");
 compra3.textContent="comprar";
 producto.appendChild(compra3);
+
+const producto4 = document.createElement("div");
+producto4.classList.add("producto");
+main.appendChild(producto4);
+
+const nombrePro4 = document.createElement("h3");
+nombrePro4.textContent = producto4.nombre;
+producto4.appendChild(nombrePro4);
+
+const foto4 = document.getElementById("image");
+producto4.appendChild(foto4);
+    
+const precioPro4 = document.createElement("p");
+precioPro4.textContent = `Precio: $${producto4.precio}`;
+producto4.appendChild(precioPro4);
+
+const compra4= document.getElementById("boton");
+compra4.textContent="comprar";
+producto4.appendChild(compra4);
+/////////////////////////
+const producto5 = document.createElement("div");
+producto5.classList.add("producto");
+main.appendChild(producto5);
+
+const nombrePro5 = document.createElement("h3");
+nombrePro5.textContent = producto5.nombre;
+producto5.appendChild(nombrePro5);
+
+const foto5 = document.getElementById("image");
+producto5.appendChild(foto5);
+    
+const precioPro5 = document.createElement("p");
+precioPro5.textContent = `Precio: $${producto5.precio}`;
+producto5.appendChild(precioPro5);
+
+const compra5= document.getElementById("boton");
+compra5.textContent="comprar";
+producto5.appendChild(compra5);
 
 
 main.innerHTML= ir();
